85afa032a9adbf5cf7981bfbba659620981a25fc
Shoma Takami <hawklook.2331@gmail.com>
2019-10-24 11:02:32 -0700

    https

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
     );
     const data = await api_call.json();
     if (city && country) {
